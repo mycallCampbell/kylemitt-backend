39d5933b4c8cd1da8ddb6cfeaab3e2be34a7c49e
unknown <mycallcampbell@gmail.com>
2021-10-10 21:46:09 +0100

    created core app, db models, views, urls and env variables

---
+++ kylemitt/core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('users/login', views.MyTokenObtainPairView.as_view(),
+    #      name='token_obtain_pair'),
+    path('', views.getRoutes, name='routes'),
+    # path('users/profile', views.getUserProfile, name='user-profile'),
+    path('products/', views.getProducts, name='products'),
+    path('product/<str:pk>', views.getProduct, name='product'),
+    path('client-secret/', views.getClientSecret, name='client-secret'),
+    path('webhook/stripe', views.stripe_webhook, name='stripe-webhook'),
+    path('addorder/', views.addOrderItems, name='add-order'),
+]
