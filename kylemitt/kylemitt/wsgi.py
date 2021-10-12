0836bfaef59b7bcef01b149bca6e5b174407a5d4
unknown <mycallcampbell@gmail.com>
2021-10-12 18:33:29 +0100

    revert wsgi file

--- kylemitt/kylemitt/wsgi.py
+++ kylemitt/kylemitt/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 
 
 dotenv.load_dotenv(
-    os.path.join(os.path.dirname(__file__), '.env')
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
 )
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
